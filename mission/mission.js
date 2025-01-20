bffb5d227854ec7f7ec2c1acc73235ddb17dd1ca
Tyler-Burke2 <smilertyler@byui.edi>
2025-01-20 10:24:52 -0700

    all done except logo

---
+++ mission/mission.js
@@ -0,0 +1,19 @@
+const themeSelector = document.querySelector('select');
+
+themeSelector.addEventListener('change', changeTheme);
+
+function changeTheme() {
+  const body = document.body;
+  const logo = document.querySelector('#logo'); 
+  const selectedTheme = themeSelector.value;
+
+  if (selectedTheme === 'dark') {
+    body.classList.add('dark');
+    logo.src = "byui-logo_white.png";
+  } else {
+    body.classList.remove('dark');
+    logo.src = 'byui-logo_white.webp'; 
+  }
+}
+
+themeSelector.addEventListener('change', changeTheme);
