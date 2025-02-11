f31b24dd38b83a196ebc475f266175b3eba00370
Tyler-Burke2 <smilertyler@byui.edi>
2025-02-11 14:40:46 -0700

    fixed

---
+++ week6.js
@@ -0,0 +1,56 @@
+//  arrays.js
+const steps = ["one", "two", "three"];
+function listTemplate(step) {
+  return `<li>${step}</li>`
+}
+const stepsHtml = steps.map(listTemplate);// use map to convert the list from strings to HTML
+document.querySelector("#myList").innerHTML = stepsHtml.join('');// set the innerHTML
+
+let grades = ['A', 'B', 'A', 'C'];
+let point;
+let gpaPoints = grades.map(function(grade) {
+    switch (grade){
+        case 'A':
+            point = 4;
+            break;
+        case 'B':
+            point = 3;
+            break;
+        case 'C':
+            point = 2;
+            break;
+        case 'D':
+            point = 1;
+            break;
+        case 'F':
+            point = 0;
+        default:
+            alert('not a valid grade');
+    }
+    return point;
+})
+
+console.log(gpaPoints);
+
+let totalPoint = gpaPoints.reduce(getTotal);
+
+function getTotal(total, item){
+    return total + item;
+}
+
+console.log(totalPoint);
+
+let gpaAverage = totalPoint/gpaPoints.length;
+
+console.log(gpaAverage);
+
+let fruit = ['watermelon', 'peach', 'apple', 'tomato', 'grape'];
+
+// Lines 50- 52 perform the same thing as the comment in line 54.
+let shortFruit = fruit.filter(function(item){
+    return item.length < 6;
+})
+
+// let shortFruit = fruit.filter((item)=> item.length < 6);
+
+console.log(shortFruit);
